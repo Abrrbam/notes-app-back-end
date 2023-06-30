2ee4449579b5b67c8ef6766ef4fc8b280895ac74
Abraham <abrahamputrabs@gmail.com>
2023-06-30 14:47:16 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,13 @@
+const notes = [];
+
+module.exports = notes;
+
+// {
+//     id: String,
+//     title: string,
+//     createdAt: string,
+//     updatedAt: string,
+//     tags: arrat of string,
+//     body: string,
+
+// },
